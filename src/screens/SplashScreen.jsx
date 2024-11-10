060b79c2dcc5da384fc4b7844a8cebfb73c6f019
Satish Kumar Sharma <skwebserv@gmail.com>
2024-11-10 18:46:23 +0530

    Added name in logo

--- src/screens/SplashScreen.jsx
+++ src/screens/SplashScreen.jsx
@@ -1,13 +1,11 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import Logo from './components/Logo';
 
 const SplashScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Text style={styles.finText}>Fin</Text>
-        <Text style={styles.bookText}>Book</Text>
-      </View>
+      <Logo />
     </SafeAreaView>
   )
 }
@@ -19,19 +17,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  logoContainer: {
-    flexDirection: 'row',
-  },
-  finText: {
-    color: 'red',
-    fontSize: 50,
-    fontWeight: '600',
-  },
-  bookText: {
-    color: '#2196F3',
-    fontSize: 50,
-    fontWeight: '600',
-  },
+
 });
 
 export default SplashScreen
--- src/screens/components/Logo.jsx
+++ src/screens/components/Logo.jsx
@@ -1,41 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 const Logo = () => {
 
   return (
-    // <View style={styles.container}>
-    //   <View style={styles.logoContainer}>
-    //     <Text style={styles.finText}>Fin</Text>
-    //     <Text style={styles.bookText}>Book</Text>
-    //   </View>
-    // </View>
-    <View style={{ display: 'flex', flexDirection: "row" }}>
-      <Text style={{ fontSize: 50, fontWeight: 600, color: 'red' }}>Fin</Text>
-      <Text style={{ fontSize: 50, fontWeight: 600, color: '#2196f3' }}>Book</Text>
+    <View>
+      <View style={{ display: 'flex', flexDirection: "row" }}>
+        <Text style={{ fontSize: 50, fontWeight: 600, color: 'red' }}>Fin</Text>
+        <Text style={{ fontSize: 50, fontWeight: 600, color: '#2196f3' }}>Book</Text>
+      </View>
+      <Text style={{ textAlign: "center", color: 'gray' }}>By Satish Kumar Sharma</Text>
     </View>
   )
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  logoContainer: {
-    flexDirection: 'row',
-  },
-  finText: {
-    color: 'red',
-    fontSize: 50,
-    fontWeight: '600',
-  },
-  bookText: {
-    color: '#2196F3',
-    fontSize: 50,
-    fontWeight: '600',
-  },
-});
 
 export default Logo
